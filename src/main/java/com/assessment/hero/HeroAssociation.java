04489c2fea28d6350a3b617b13d63115b5549253
Babacar <babacarthierry@gmail.com>
2019-09-16 18:33:06 -0400

    Swagger config

--- src/main/java/com/assessment/hero/HeroAssociation.java
+++ src/main/java/com/assessment/hero/HeroAssociation.java
@@ -3,8 +3,10 @@ package com.assessment.hero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan({"com.assessment.hero"})
 public class HeroAssociation {
     public static void main(String[] args) {
