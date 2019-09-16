db07174f91d8144839d78e07f54a8a8d2626d038
Babacar <babacarthierry@gmail.com>
2019-09-16 01:30:35 -0400

    Create-Hero - hero/create endpoint calls service to create hero

--- src/main/java/com/assessment/hero/controller/HeroController.java
+++ src/main/java/com/assessment/hero/controller/HeroController.java
@@ -1,6 +1,8 @@
 package com.assessment.hero.controller;
 
 import com.assessment.hero.model.Hero;
+import com.assessment.hero.service.HeroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HeroController {
 
+    private final HeroService heroService;
+
+    @Autowired
+    public HeroController(HeroService heroService) {
+        this.heroService = heroService;
+    }
+
     @PostMapping("/hero/create")
     public ResponseEntity<String> createHero(@RequestBody Hero hero) {
+        heroService.create(hero);
         return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/assessment/hero/service/HeroService.java
+++ src/main/java/com/assessment/hero/service/HeroService.java
@@ -1,7 +1,9 @@
 package com.assessment.hero.service;
 
 import com.assessment.hero.model.Hero;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HeroService {
 
     public void create(Hero hero){
